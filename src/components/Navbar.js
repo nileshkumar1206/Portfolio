caf419719d81098b82e4f647b52f8fa0531de416
nileshkumar1206 <96872150+nileshkumar1206@users.noreply.github.com>
2024-05-27 16:57:59 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/nileshkumar1206"
+                href="https://github.com/nileshkumar1206/Portfolio.git"
                 target="_blank"
                 className="fork-btn-inner"
               >
